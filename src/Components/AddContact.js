f1af89660bb85d2ec298bfc3664a1be8f472a5a8
ShreyaSingh10 <shreyasssingh10gmail.com>
2018-08-03 14:39:09 +0530

    Added prettier to format the code

--- src/Components/AddContact.js
+++ src/Components/AddContact.js
@@ -1,102 +1,132 @@
 import React from 'react';
-import "./styles.css"
+import './styles.css';
 
 import { phoneValidator } from '../helpers/validators';
 
-import defaultProfileImage from '../dummy.jpeg'
+import defaultProfileImage from '../dummy.jpeg';
 
-class AddContact extends React.Component{
-    state={
-    	name:'',
-    	number:'',
-    	image:null,
-    	fileindex:null,
-    	phoneError: false,
-    	nameError: false,
+class AddContact extends React.Component {
+  state = {
+    name: '',
+    number: '',
+    image: null,
+    fileindex: null,
+    phoneError: false,
+    nameError: false
+  };
+
+  handleChangeName = e => {
+    const name = e.target.value;
+    this.setState({
+      name,
+      nameError: !(name.length > 0)
+    });
+  };
+
+  handleChangeNumber = e => {
+    const number = e.target.value;
+    this.setState({
+      number,
+      phoneError: !phoneValidator(number)
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, number, image } = this.state;
+
+    const phoneError = !phoneValidator(number);
+    const nameError = !(name.length > 0);
+
+    if (phoneError) {
+      this.setState({
+        phoneError
+      });
+      return;
     }
 
-	handleChangeName= e =>{
-		const name= e.target.value;
-        this.setState({
-        	name,
-        	nameError: !((name.length)> 0)
-        });
-	}
+    if (nameError) {
+      this.setState({
+        nameError
+      });
+      return;
+    }
 
-	handleChangeNumber= e =>{
-	   const number= e.target.value;
-		this.setState({
-			number,
-			phoneError:!phoneValidator(number)
-		})
-	}
+    const contact = {
+      name,
+      number,
+      image: image ? image : defaultProfileImage
+    };
 
-	handleSubmit = e =>{
-		e.preventDefault();
-		const { name, number, image } = this.state;
+    this.props.addingContact(contact);
+    this.setState({
+      name: '',
+      number: '',
+      image: '',
+      fileindex: null,
+      phoneError: false,
+      nameError: false
+    });
+  };
+  fileChangedHandler = event => {
+    const file = event.target.files[0];
+    event.target.value = null;
+    // coverting to b64
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      this.setState({ image: reader.result });
+    };
+    reader.readAsDataURL(file);
+  };
 
-		const phoneError = !phoneValidator(number)
-		const nameError =  !(name.length>0)
-
-
-		if(phoneError){
-			this.setState({
-				phoneError
-			})
-			return;
-		}
-
-		if(nameError){
-			this.setState({
-							nameError
-						})
-			return;
-		}
-
-		const contact ={
-					name,
-					number,
-					image:(image?image:defaultProfileImage)
-				}
-
-	            this.props.addingContact(contact);
-				this.setState({
-		       		name:'',
-		       		number:'',
-		       		image:'',
-		       		fileindex:null,
-		       		phoneError:false,
-		       		nameError:false
-		       	})
-
-	}
-    fileChangedHandler = (event) => {
-        const file = event.target.files[0];
-        event.target.value =null;
-        // coverting to b64
-        const reader = new FileReader();
-  		reader.onloadend = () => {
-    		this.setState({image:reader.result });
-  		}
-  		reader.readAsDataURL(file);
-	}
-
-	render(){
-		const {name,number,image,nameError,phoneError} = this.state;
-		return(
-			<div className="addContacts">
-				<form onSubmit={this.handleSubmit}>
-        <div>
-						{(image)?<img className="picture" src={image} align="middle"/> :<input className="file-upload" type="file" onChange={this.fileChangedHandler}  /> }
-        </div>
-				<div><input onChange={this.handleChangeName} value ={name} type="text" placeholder="Enter name"></input></div>
-					{nameError && <p><b>Enter a valid name</b></p> }
-				<div><input onChange={this.handleChangeNumber} value={number} type="tel" placeholder="Enter number"></input></div>
-					{phoneError && <p><b>Enter a valid number</b></p>}
-				<div><button type="submit" >Submit</button></div>
-				</form>
-			</div>
-		)
-	}
+  render() {
+    const { name, number, image, nameError, phoneError } = this.state;
+    return (
+      <div className="addContacts">
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            {image ? (
+              <img className="picture" src={image} align="middle" />
+            ) : (
+              <input
+                className="file-upload"
+                type="file"
+                onChange={this.fileChangedHandler}
+              />
+            )}
+          </div>
+          <div>
+            <input
+              onChange={this.handleChangeName}
+              value={name}
+              type="text"
+              placeholder="Enter name"
+            />
+          </div>
+          {nameError && (
+            <p>
+              <b>Enter a valid name</b>
+            </p>
+          )}
+          <div>
+            <input
+              onChange={this.handleChangeNumber}
+              value={number}
+              type="tel"
+              placeholder="Enter number"
+            />
+          </div>
+          {phoneError && (
+            <p>
+              <b>Enter a valid number</b>
+            </p>
+          )}
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 export default AddContact;
--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,64 +1,71 @@
-import React from "react";
-import AddContact from "./AddContact";
-import DisplayContacts from "./DisplayContacts"
+import React from 'react';
+import AddContact from './AddContact';
+import DisplayContacts from './DisplayContacts';
 
-class Container extends React.Component{
+class Container extends React.Component {
+  state = {
+    contacts: []
+  };
 
-    state={
-    	contacts:[]
+  addingContact = contact => {
+    this.setState(prevstate => {
+      let a = prevstate.contacts;
+      a.push(contact);
+      localStorage.setItem('contacts', JSON.stringify(a));
+      return {
+        contacts: a
+      };
+    });
+  };
+
+  deletingContact = number => {
+    this.setState(prevstate => {
+      const newContacts = prevstate.contacts.filter(c => c.number !== number);
+      localStorage.setItem('contacts', JSON.stringify(newContacts));
+      return {
+        contacts: newContacts
+      };
+    });
+  };
+
+  componentDidMount() {
+    let localContacts = localStorage.getItem('contacts');
+    if (localContacts) {
+      this.setState({
+        contacts: JSON.parse(localContacts)
+      });
     }
+  }
 
-    addingContact = (contact) => {
-    	this.setState((prevstate) =>{
-    		let a= prevstate.contacts;
-    		a.push(contact);
-    		localStorage.setItem("contacts",JSON.stringify(a));
-    		return {
-    			contacts:a
-    		}
-    	})
-    }
-
-	deletingContact = (number) => {
-		this.setState((prevstate)=> {
-			const newContacts= prevstate.contacts.filter( (c) => c.number !== number )
-			localStorage.setItem("contacts", JSON.stringify(newContacts));
-			return {
-				contacts: newContacts
-			}
-		})
-	}
-
-	componentDidMount(){
-		let localContacts = localStorage.getItem("contacts");
-		if(localContacts){
-				this.setState({
-					contacts:JSON.parse(localContacts)
-				})
-			}
-	}
-
-	render(){
-		return(
-			<div className="container">
-        <h1><font face = "Comic sans MS" size ="7">Contacts App</font></h1>
-				<AddContact addingContact={this.addingContact}/>
-        <h2><font face = "Comic sans MS" size =" 5">Contacts List</font></h2>
+  render() {
+    return (
+      <div className="container">
+        <h1>
+          <font face="Comic sans MS" size="7">
+            Contacts App
+          </font>
+        </h1>
+        <AddContact addingContact={this.addingContact} />
+        <h2>
+          <font face="Comic sans MS" size=" 5">
+            Contacts List
+          </font>
+        </h2>
         <div className="ancestor">
           <div className="parent_contanier">
-    				{this.state.contacts.map((info)=>
-    					<DisplayContacts
-    						key={info.number}
-    						name={info.name}
-    						number={info.number}
-    						image={info.image}
-    						deletingContact={this.deletingContact}
-    					/>
-    				)}
+            {this.state.contacts.map(info => (
+              <DisplayContacts
+                key={info.number}
+                name={info.name}
+                number={info.number}
+                image={info.image}
+                deletingContact={this.deletingContact}
+              />
+            ))}
           </div>
-			</div>
-    </div>
-		)
-	}
+        </div>
+      </div>
+    );
+  }
 }
 export default Container;
