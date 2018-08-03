b36200046fe7b838a75b3127b1b4d74f1d8f92cf
ShreyaSingh10 <shreyasssingh10gmail.com>
2018-08-03 15:03:44 +0530

    Formatting some changes in the code

--- src/Components/DisplayContacts.js
+++ src/Components/DisplayContacts.js
@@ -1,22 +1,29 @@
-import React from "react";
-import "./styles.css"
+import React from 'react';
+import './styles.css';
 
-class DisplayContacts extends React.Component{
+class DisplayContacts extends React.Component {
+  render() {
+    const { deletingContact } = this.props;
+    const { image, name, number } = this.props;
 
-	render(){
-		const { deletingContact } = this.props;
-		const { image, name,number } = this.props;
-
-		return(
-
-			<div className="contacts_contanier">
-			    <img src={image} className="picture"/>
-				<div><font face = "Comic sans MS" size =" 2">Name: {name}</font></div>
-				<div><font face = "Comic sans MS" size =" 2">Number: {number}</font></div>
-				<button className="delete" onClick={() => deletingContact(number)} >Delete</button>
-			</div>
-
-		)
-	}
+    return (
+      <div className="contacts_contanier">
+        <img src={image} className="picture" />
+        <div>
+          <font face="Comic sans MS" size=" 2">
+            Name: {name}
+          </font>
+        </div>
+        <div>
+          <font face="Comic sans MS" size=" 2">
+            Number: {number}
+          </font>
+        </div>
+        <button className="delete" onClick={() => deletingContact(number)}>
+          Delete
+        </button>
+      </div>
+    );
+  }
 }
 export default DisplayContacts;
